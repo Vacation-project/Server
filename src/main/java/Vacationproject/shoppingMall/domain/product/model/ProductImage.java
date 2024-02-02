e048115fb258623c079a973c397e33f22920b358
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-02-02 22:08:29 +0900

    feat: ProductImage 엔티티 개발 및 연관관계 설정 메서드 생성

---
+++ src/main/java/Vacationproject/shoppingMall/domain/product/model/ProductImage.java
@@ -0,0 +1,38 @@
+package Vacationproject.shoppingMall.domain.product.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @NotNull
+    @URL /* 올바른 url 형식이 아니라면 검증 부분에서 에러를 발생시킴 */
+    private String imageUrl;
+
+    public static ProductImage of(Product product, String imageUrl) {
+        ProductImage productImage = ProductImage.builder()
+                .product(product) //연관관계 설정
+                .imageUrl(imageUrl)
+                .build();
+
+        product.getProductImageList().add(productImage); //연관관계 설정
+
+        return productImage;
+    }
+}
