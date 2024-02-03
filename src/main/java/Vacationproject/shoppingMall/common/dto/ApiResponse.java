fa2f071a764c62def513763bc701bfaacf51f82e
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-02-03 18:15:05 +0900

    refactor: ApiReponse 공통 응답 포맷에 data가 필요없는 success 응답 개발

--- src/main/java/Vacationproject/shoppingMall/common/dto/ApiResponse.java
+++ src/main/java/Vacationproject/shoppingMall/common/dto/ApiResponse.java
@@ -11,23 +11,21 @@ import java.util.HashMap;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
+
+    private static final String SUCCESS_STATUS = "success";
+
     private Integer statusCode;
+    private String status;
     private T data;
     private String message;
 
-    private ApiResponse(T data) {
-        this.statusCode = null;
-        this.data = data;
-    }
-
-    private ApiResponse(int status, T data, String message) {
-        this.statusCode = status;
-        this.data = data;
-        this.message = message;
-    }
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(data);
+        return new ApiResponse<>(SUCCESS_STATUS, data);
+    }
+
+    public static <T> ApiResponse<T> successWithNoContent() {
+        return new ApiResponse<>(SUCCESS_STATUS, null);
     }
 
     public static ApiResponse error(int errorCode, String message) {
@@ -37,5 +35,21 @@ public class ApiResponse<T> {
 
     public static <T> ApiResponse<T> error(int errorCode, T data, String message) {
         return new ApiResponse<>(errorCode, data, message);
+    }
+
+    private ApiResponse(String status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    private ApiResponse(int status, T data) {
+        this.statusCode = null;
+        this.data = data;
+    }
+
+    private ApiResponse(int status, T data, String message) {
+        this.statusCode = status;
+        this.data = data;
+        this.message = message;
     }
 }
