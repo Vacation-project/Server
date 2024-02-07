8ea07d56a4873c26ba24e5e4d9bf6d86389237dc
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-02-08 00:57:19 +0900

    feat: 각 PathVariable에 대한 정보를 @Parameter로 추가해서 어떤 정보인지 명시화

--- src/main/java/Vacationproject/shoppingMall/domain/product/api/ProductApiController.java
+++ src/main/java/Vacationproject/shoppingMall/domain/product/api/ProductApiController.java
@@ -4,6 +4,8 @@ import Vacationproject.shoppingMall.common.constant.SwaggerConstants;
 import Vacationproject.shoppingMall.common.dto.ApiResponse;
 import Vacationproject.shoppingMall.domain.product.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -42,7 +44,7 @@ public class ProductApiController {
             @RequestPart(value = "createProductRequest") @Valid final CreateProductRequest createProductRequest,
             @NotNull @RequestPart(value = "images") List<MultipartFile> images,
 //                                     @AuthenticationPrincipal PrincipalDetails principalDetails,
-            @PathVariable(name = "categoryId") final Long categoryId) throws IOException {
+            @Parameter(name = "categoryId", description = "Category 의 id", in = ParameterIn.PATH)@PathVariable(name = "categoryId") final Long categoryId) throws IOException {
         /* 로그인한 유저가 어드민이 맞는지 검증 */
         // authService.checkIsAdmin(principalDetails.getUser())
 
@@ -56,8 +58,8 @@ public class ProductApiController {
     @GetMapping("/{productId}")
     @Operation(summary = "상품 정보 페이지", description = "ProductId와 일치하는 상품 정보를 가져옵니다.")
     public ApiResponse<ProductDetailResponse> getProduct(
-            @PathVariable(name = "productId") final Long productId,
-            @PageableDefault(page = 0, size=4, sort = "id", direction = Sort.Direction.DESC) final Pageable pageable
+            @Parameter(name = "productId", description = "Product 의 id", in = ParameterIn.PATH) @PathVariable(name = "productId") final Long productId,
+            @Parameter(name = "상품 페이징 정보", description = "전송하지 않아도 됩니다.") @PageableDefault(page = 0, size=4, sort = "id", direction = Sort.Direction.DESC) final Pageable pageable
     ) {
         final ProductDetailResponse productDetailResponse = productService.getProductAndReview(productId, pageable);
         return success(productDetailResponse);
@@ -69,7 +71,7 @@ public class ProductApiController {
     @GetMapping("/{productId}/admin")
     @Operation(summary = "상품 수정 폼", description = "productId와 일치하는 상품의 정보를 가져옵니다.")
     public ApiResponse<ProductUpdateResponse> updateProductForm(
-            @PathVariable(name = "productId") final Long productId
+            @Parameter(name = "productId", description = "Product 의 id", in = ParameterIn.PATH) @PathVariable(name = "productId") final Long productId
 //            @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
         // authService.checkIsAdmin(principalDetails.getUser())
@@ -81,7 +83,7 @@ public class ProductApiController {
     @PutMapping("/{productId}/admin")
     @Operation(summary = "상품 수정", description = "수정한 상품 정보와 상품 이미지(images)를 이용하여 상품 정보를 수정합니다.")
     public ApiResponse<ProductMessage> updateProduct(
-            @PathVariable(name = "productId") final Long productId,
+            @Parameter(name = "productId", description = "Product 의 id", in = ParameterIn.PATH) @PathVariable(name = "productId") final Long productId,
             @RequestBody @Valid final UpdateProductRequest updateProductRequest
 //            @AuthenticationPrincipal PrincipalDetails principalDetails
     ) throws IOException {
@@ -97,7 +99,7 @@ public class ProductApiController {
     @DeleteMapping("/{productId}/admin")
     @Operation(summary = "상품 삭제", description = "ProductId와 일치하는 상품을 삭제합니다.")
     public ApiResponse<ProductMessage> deleteProduct(
-            @PathVariable(name = "productId") final Long productId
+            @Parameter(name = "productId", description = "Product 의 id", in = ParameterIn.PATH) @PathVariable(name = "productId") final Long productId
 //            @AuthenticationPrincipal PrincipalDetails principalDetails
     ) {
         // authService.checkIsAdmin(principalDetails.getUser())
