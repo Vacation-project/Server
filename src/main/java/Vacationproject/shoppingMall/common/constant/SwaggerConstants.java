0914df7618c79c46e48af4fda3c5f2ebd267cb9a
Psm_01 <71458064+pp8817@users.noreply.github.com>
2024-02-07 15:04:27 +0900

    오타 수정

--- src/main/java/Vacationproject/shoppingMall/common/constant/SwaggerConstants.java
+++ src/main/java/Vacationproject/shoppingMall/common/constant/SwaggerConstants.java
@@ -9,7 +9,7 @@ public class SwaggerConstants {
     public static final String[] SWAGGER_APPOINTED_PATHS = {
             "/**"
     };
-    public static final String DEFINITION_TITLE = "Vacation Projdct API 먕세서";
+    public static final String DEFINITION_TITLE = "Vacation Projdct API 명세서";
     public static final String DEFINITION_DESCRIPTION = "\uD83D\uDE80 Realtime Congestion Based Location Recommendation Service - Vacation Project Server의 API 명세서입니다.";
     public static final String DEFINITION_VERSION = "v1";
 
